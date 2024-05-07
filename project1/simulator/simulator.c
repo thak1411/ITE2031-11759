a5d2707b08a997f162649c8dc197740349414320
thak1411 <thak1411@gmail.com>
2024-05-07 21:14:56 +0900

    Add: Simulator

--- project1/simulator/simulator.c
+++ project1/simulator/simulator.c
@@ -19,6 +19,7 @@ int convertNum(int num);
 
 int main(int argc, char *argv[])
 {
+    int is_halted, ir, opcode, reg_a, reg_b, reg_dest, offset_field, executed_instructions;
     char line[MAXLINELENGTH];
     stateType state;
     FILE *filePtr;
@@ -46,8 +47,48 @@ int main(int argc, char *argv[])
         printf("memory[%d]=%d\n", state.numMemory, state.mem[state.numMemory]);
     }
 
-		/* TODO: */
-    return(0);
+    state.pc = 0;
+    memset(state.reg, 0, sizeof state.reg);
+
+    for (executed_instructions = 0; ; ) {
+        printState(&state);
+        ir = state.mem[state.pc++];
+        ++executed_instructions;
+
+        opcode = ir >> 22;
+        reg_a = (ir >> 19) & 0x7;
+        reg_b = (ir >> 16) & 0x7;
+        reg_dest = ir & 0x7;
+        offset_field = convertNum(ir & 0xFFFF);
+        if (opcode == 0) { // add
+            state.reg[reg_dest] = state.reg[reg_a] + state.reg[reg_b];
+        } else if (opcode == 1) { // nor
+            state.reg[reg_dest] = ~(state.reg[reg_a] | state.reg[reg_b]);
+        } else if (opcode == 2) { // lw
+            state.reg[reg_b] = state.mem[state.reg[reg_a] + offset_field];
+        } else if (opcode == 3) { // sw
+            state.mem[state.reg[reg_a] + offset_field] = state.reg[reg_b];
+        } else if (opcode == 4) { // beq
+            state.pc += (state.reg[reg_a] == state.reg[reg_b]) ? offset_field : 0;
+        } else if (opcode == 5) { // jalr
+            state.reg[reg_b] = state.pc;
+            state.pc = state.reg[reg_a];
+        } else if (opcode == 6) { // halt
+            break;
+        } else if (opcode == 7) { // noop
+            continue;
+        } else {
+            printf("error: invalid opcode\n");
+            exit(1);
+        }
+    }
+
+    printf("machine halted\n");
+    printf("total of %d instructions executed\n", executed_instructions);
+    printf("final state of machine:\n");
+
+    printState(&state);
+    return 0;
 }
 
 void printState(stateType *statePtr)
