7b3811fd80ad2c816e1e1814ac44f10308abadb8
thak1411 <thak1411@gmail.com>
2024-05-07 23:14:29 +0900

    Add: Runner Shell Documents

--- project1/run.sh
+++ project1/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Usage: sh run.sh <file_name>
+#  Example: sh run.sh test1
+
 cd assembler && make && ./assembler $1.as $1.mc && cp $1.mc ../simulator/$1.mc && cd .. &&
     cd simulator && make && ./simulator $1.mc > $1.out.result
